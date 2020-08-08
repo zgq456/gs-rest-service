0b7e6d9cd35418991a9d47186ba96de81a2a1453
zgq <zgq456@qq.com>
2020-08-08 08:09:00 +0800

    optimise

--- complete/docker/run.sh
+++ complete/docker/run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 service ssh start
-sh /install-repeater.sh
+curl -s http://sandbox-ecological.oss-cn-hangzhou.aliyuncs.com/install-repeater.sh | bash
 cp /repeater.properties /root/.sandbox-module/cfg/
 java -Djava.security.egd=file:/dev/./urandom -Dapp.name=gs-service -Dapp.env=daily -jar /app.jar
 #bash ~/sandbox/bin/sandbox.sh -p `ps -ef | grep "app.jar" | grep -v grep | awk '{print $2}'` -P 12580
