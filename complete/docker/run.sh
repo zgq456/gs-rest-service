da800b917d094ae85b016ba96341a4a8b7521a1d
Zhang Guoqi <gzhang@metareco.com>
2020-08-09 22:01:29 +0800

    optimise

--- complete/docker/run.sh
+++ complete/docker/run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+#export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 service ssh start
 #curl -s http://sandbox-ecological.oss-cn-hangzhou.aliyuncs.com/install-repeater.sh | bash
 #cp /repeater.properties /root/.sandbox-module/cfg/
