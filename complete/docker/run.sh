bbbd255a6570b23612ef38ba3b4c62c10a6b1e23
Zhang Guoqi <gzhang@metareco.com>
2020-08-09 22:51:33 +0800

    optimise

--- complete/docker/run.sh
+++ complete/docker/run.sh
@@ -3,5 +3,5 @@
 service ssh start
 #curl -s http://sandbox-ecological.oss-cn-hangzhou.aliyuncs.com/install-repeater.sh | bash
 #cp /repeater.properties /root/.sandbox-module/cfg/
-java -Djava.security.egd=file:/dev/./urandom -Dapp.name=gs-service -Dapp.env=daily -jar /app.jar
+java -Djava.security.egd=file:/dev/./urandom -Dapp.name=gs-service -Dapp.env=test -jar /app.jar
 #bash ~/sandbox/bin/sandbox.sh -p `ps -ef | grep "app.jar" | grep -v grep | awk '{print $2}'` -P 12580
