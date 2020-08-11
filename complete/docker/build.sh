096bc14cea0319334253cfd03894437323daa77b
Zhang Guoqi <gzhang@metareco.com>
2020-08-11 19:21:47 +0800

    optimise

--- complete/docker/build.sh
+++ complete/docker/build.sh
@@ -2,7 +2,7 @@ docker build -t springbootdemo4docker .
 
 docker rm -f gs-service || true
 
-docker run --name gs-service -d -p 18080:8080 -p 10122:22 springbootdemo4docker
+docker run --name gs-service -d -p 18080:8080 -p 10122:22 -p 12580:12580 springbootdemo4docker
 
 
 
